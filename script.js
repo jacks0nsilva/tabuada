b2b67d0897f61d77cd1b12bb1f25ceeff46e2faa
J4CKS0NBR <jacksonsilva_fsa@outlook.com>
2023-01-08 14:35:35 -0300

    correção

--- script.js
+++ script.js
@@ -9,13 +9,13 @@ function GerarTabuada(){
     if (numero2 <= 0){
         window.alert('Por favor, digite um número válido!')
     } else {
-       // resultado.innerHTML = `<p>Tabuada de: <strong>${numero2}</strong></p>`
-       tab.innerHTML = ``
+       resultado.innerHTML = `<p>Tabuada de: <strong>${numero2}</strong></p>`
+       //tab.innerHTML = ``
         for (let c = 1 ; c <= 10 ; c++){
-            let item = document.createElement('option')
-            item.text = `${numero2} x ${c} = ${numero2 * c}`
-            tab.appendChild(item)
-            //resultado.innerHTML += `<p>${numero2} x ${c} = <strong>${numero2 * c}</strong> </p>`
+           // let item = document.createElement('option')
+           // item.text = `${numero2} x ${c} = ${numero2 * c}`
+           // tab.appendChild(item)
+            resultado.innerHTML += `<p>${numero2} x ${c} = <strong>${numero2 * c}</strong> </p>`
         }
     }
 
